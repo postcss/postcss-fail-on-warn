c96af94c04e5a320820a37537cd2e59615224feb
Andrey Sitnik <andrey@sitnik.ru>
2021-06-09 17:34:52 -0400

    Improve warning print

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const warningsReporter = () => {
+module.exports = () => {
   return {
     postcssPlugin: 'postcss-fail-on-warn',
-    OnceExit (css, { result }) {
+    OnceExit(css, { result, CssSyntaxError }) {
       let [first, ...rest] = result.warnings()
+      if (!first) return
 
-      /* eslint-disable no-console */
-      rest && rest.map(console.log)
-
-      if (first) {
-        throw new Error(first)
+      function format(warning) {
+        if (warning.line) {
+          let input = (warning.node && warning.node.input) || {}
+          let formatter = new CssSyntaxError(
+            warning.text,
+            warning.line,
+            warning.column,
+            input.css,
+            input.file,
+            warning.plugin
+          )
+          return formatter.toString().replace(/^CssSyntaxError: /, '')
+        } else if (warning.plugin) {
+          return warning.plugin + ': ' + warning.text
+        } else {
+          return warning.text
+        }
       }
+
+      if (rest) {
+        for (let warning of rest) {
+          console.warn(format(warning))
+        }
+      }
+
+      throw new Error(format(first))
     }
   }
 }
-
-module.exports = warningsReporter
 module.exports.postcss = true
--- index.test.js
+++ index.test.js
@@ -2,19 +2,31 @@ let postcss = require('postcss')
 
 let plugin = require('./')
 
-function run (another, opts) {
+function run(another, opts) {
   return function () {
-    return postcss([another, plugin(opts)]).process('').css
+    return postcss([another, plugin(opts)]).process('a{}').css
   }
 }
 
 it('does not do anything without warnings', () => {
-  expect(run(() => { })).not.toThrow()
+  expect(run(() => {})).not.toThrow()
 })
 
-it('throw first warning', () => {
-  expect(run((root, result) => {
-    result.warn('Test 1')
-    result.warn('Test 2')
-  })).toThrow('Test 1')
+it('throws first warning', () => {
+  jest.spyOn(console, 'warn').mockImplementation(() => {})
+  expect(
+    run((root, result) => {
+      root.warn(result, 'Test 1', { plugin: 'postcss-test' })
+      result.warn('Test 2')
+      result.warn('Test 3', { plugin: 'postcss-test' })
+      root.warn(result, 'Test 4', { plugin: 'postcss-test' })
+    })
+  ).toThrow('Test 1')
+  expect(console.warn).toHaveBeenCalledTimes(3)
+  expect(console.warn).toHaveBeenNthCalledWith(1, 'Test 2')
+  expect(console.warn).toHaveBeenNthCalledWith(2, 'postcss-test: Test 3')
+  expect(console.warn).toHaveBeenNthCalledWith(
+    3,
+    'postcss-test: <css input>:1:1: Test 4'
+  )
 })
